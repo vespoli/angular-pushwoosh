a6167555767b22c4416bba54acd22445f02086aa
David Vespoli <david.vespoli@gmail.com>
2015-07-01 13:01:28 -0700

    Updated for cordova.

--- src/angular-pushwoosh.js
+++ src/angular-pushwoosh.js
@@ -2,6 +2,7 @@
   'use strict';
   angular.module('pushwooshNotification', [])
   .provider('$pushNotification', function() {
+    var isRegistered = false;
     var settings = {
       appId: null,
       appName: null,
@@ -23,48 +24,83 @@
         return (typeof window.plugins !== 'undefined')
                 && (typeof window.plugins.pushNotification !== 'undefined');
       },
-      getTags: function() {}
-    };
-
-    api.pushNotification = api.isAvailable() ? window.plugins.pushNotification : null;
+      setTags: function(subscriptions) {
+        if (isRegistered) {          
+          var pushNotification = cordova.require("com.pushwoosh.plugins.pushwoosh.PushNotification");          
+          pushNotification.setTags(subscriptions, function (status) {
+            console.warn("Tags set.");
+          }, function (status) {
+            console.warn("Tags not set.");
+          });
+        } else {
+          console.log('not registered');
+        }
+      }
+    };    
 
     var registerPushwoosh = function(params, callback) {
       try {
-      //set push notifications handler
-      document.addEventListener('push-notification', function(e) {
-        console.log(JSON.stringify(e.notification));
-        if (settings.onPushNotification)          
-         settings.onPushNotification(e);
-      });
+        //set push notifications handler
+        document.addEventListener('push-notification', function(e) {
+          console.log(JSON.stringify(e.notification));
+          if (settings.onPushNotification)          
+           settings.onPushNotification(e);
+        });
 
-      //start register
-      console.log('try to register for push');
-      api.pushNotification.onDeviceReady(params);
+        //start register
+        console.log('try to register for push');
+        //api.pushNotification.onDeviceReady(params);
 
-      //!!! Please note this is an API for PGB plugin. This code is different in CLI plugin!!!
-      api.pushNotification.registerDevice(function(status) {
-        var token = null;
-        console.log(typeof status);
-        if (typeof status == 'string') {//android
-          token = status;
-        } else {//ios
-          token = status['deviceToken'];
-        }
-        console.warn('push token: ' + token);
-        if (settings.onRegisterSuccess)
-          onRegisterSuccess(token);
-      }, function(status) {
-        console.warn(JSON.stringify(['failed to register ', status]));
-        if (settings.onRegisterError)
-          settings.onRegisterError(status);
-      });
-      //if (callback) callback();
+        api.pushNotification.registerDevice(function(status) {
+          var pushNotification = cordova.require("com.pushwoosh.plugins.pushwoosh.PushNotification");
+
+          if (device.platform == "Android") {
+            pushNotification.onDeviceReady({ projectid: settings.gcmProjectNumber, appid: settings.appId });
+            pushNotification.registerDevice(
+              function (token) {
+                isRegistered = true;
+                console.warn("Device registered: " + JSON.stringify(token));
+              },
+              function (status) {
+                console.warn("Registration error: " + JSON.stringify(status));
+              }
+            );
+          }
+
+          if (device.platform == "iPhone" || device.platform == "iOS") {
+            pushNotification.onDeviceReady({ pw_appid: settings.appId });
+            pushNotification.registerDevice(
+              function (status) {
+                isRegistered = true;
+                console.warn("Device registered: " + JSON.stringify(status));
+              },
+              function (status) {
+                console.warn('Registration error:' + JSON.stringify(status));
+              }
+            );
+            pushNotification.setApplicationIconBadgeNumber(0);
+          }
+
+        });
+
       } catch(err) {
         console.log(err.message);
       }
     };
 
+    var unregisterPushwoosh = function() {
+      var pushNotification = cordova.require("com.pushwoosh.plugins.pushwoosh.PushNotification");
+      pushNotification.unregisterDevice(function (status) {
+        isRegistered = false;
+        console.warn("Device unregistered: " + JSON.stringify(status));
+      },
+      function (status) {
+        console.warn('Unregistration error:' + JSON.stringify(status));
+      });
+    }
+
     this.register = function(params) {
+      if(!window.cordova){return;}
       angular.extend(settings, params);
       try {
         registerSettings.android.projectid = settings.gcmProjectNumber,
@@ -72,17 +108,20 @@
         registerSettings.ios.pw_appid = settings.appId;
 
         console.log(JSON.stringify(settings));
-        console.log('is available', api.isAvailable());
-        if (api.isAvailable()) {
+        var init = function(){
+          api.pushNotification = api.isAvailable() ? window.plugins.pushNotification : null;
+          // console.log('is available', api.isAvailable());
           if(device.platform == "Android") {
             registerPushwoosh(registerSettings.android);
           }
           if(device.platform == "iPhone" || device.platform == "iOS") {
-            registerPushwoosh(registerSettings.ios, function() {
-              //reset badges on start
-              pushNotification.setApplicationIconBadgeNumber(0);
-            });
+            registerPushwoosh(registerSettings.ios);
           }
+        }
+        if (api.isAvailable()) {
+          init();
+        } else {
+          document.addEventListener('deviceready', init, false)
         }
       } catch(err) {
         console.log(err.message);
